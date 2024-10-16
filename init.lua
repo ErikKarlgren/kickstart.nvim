34dbff17a13f4affd393ad3f375f8374753b7f8c
exeekd <exeekd@gmv.com>
2024-10-16 13:58:20 +0200

    Format python with black

--- init.lua
+++ init.lua
@@ -705,6 +705,9 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+
+        -- NOTE: Added by me
+        python = { 'black' },
       },
     },
   },
