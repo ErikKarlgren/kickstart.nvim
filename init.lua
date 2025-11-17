8a003d47643ca77f78a0200e7a24564048a2af3d
Erik Karlgren Domercq <ekarlgren.ext@gmv.com>
2025-11-17 13:53:01 +0100

    feat: ensure clipboard works with WSL1 (windows)

--- init.lua
+++ init.lua
@@ -922,6 +922,13 @@ require('lazy').setup({
   require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  {
+    -- Add better clipboard support to wsl
+    'bkoropoff/clipipe',
+    opts = {
+      start_timeout = 1000,
+    }
+  },
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
