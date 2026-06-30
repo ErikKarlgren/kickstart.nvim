47e806c734d018702ce49773af301ad6aabfaa64
Erik Karlgren Domercq <erik.karldom@gmail.com>
2026-06-30 12:39:11 +0200

    Enable bashls

--- init.lua
+++ init.lua
@@ -627,6 +627,7 @@ require('lazy').setup({
         -- gopls = {},
         pyright = {},
         rust_analyzer = {},
+        bashls = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
