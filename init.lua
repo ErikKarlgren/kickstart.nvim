29cb7209cf385c12f40d5e907fc3ddadce51684a
Erik Karlgren Domercq <erik.karldom@gmail.com>
2026-06-30 12:38:59 +0200

    Enable .nvim.lua files

--- init.lua
+++ init.lua
@@ -128,6 +128,10 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- Enable .nvim.lua files
+vim.o.exrc = true
+vim.o.secure = true
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
