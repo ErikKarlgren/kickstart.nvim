bb928aa7c12bd4371ff5848abb01497cd2ea9eda
Erik Karlgren Domercq <erik.karldom@gmail.com>
2026-08-17 12:32:50 +0200

    Delete ui{}, it seems deprecated

--- init.lua
+++ init.lua
@@ -1077,27 +1077,6 @@ require('lazy').setup {
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
   -- you can continue same window with `<space>sr` which resumes last telescope search
-  { ---@diagnostic disable-line: missing-fields
-    ui = {
-      -- If you are using a Nerd Font: set icons to an empty table which will use the
-      -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-      icons = vim.g.have_nerd_font and {} or {
-        cmd = '⌘',
-        config = '🛠',
-        event = '📅',
-        ft = '📂',
-        init = '⚙',
-        keys = '🗝',
-        plugin = '🔌',
-        runtime = '💻',
-        require = '🌙',
-        source = '📄',
-        start = '🚀',
-        task = '📌',
-        lazy = '💤 ',
-      },
-    },
-  },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
